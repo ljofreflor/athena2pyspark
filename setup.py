979b684741cbb1c3a93409369ef12c1bfca983d6
Leonardo Jofré <leonardo.jofre@gmail.com>
2017-12-07 01:41:10 -0300

    instalador de la libreria

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from setuptools import setup
 
 setup(
     name="athena2pyspark",
+    packages=["athena2pyspark"],
     version=0.1,
-    scripts=["athena2pyspark"]
+    author="exalitica",
+    autor_email = "leonardo.jofre@exalitica.com",
+    # install_requires=["pyspark", "boto3"],
+    python_requires='==2.7'
 )
-
