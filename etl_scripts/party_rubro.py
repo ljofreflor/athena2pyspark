922dd37a3b01e81d3d1cd0129373262b782415d2
Leonardo Jofré <leonardo.jofre@gmail.com>
2017-12-12 04:09:05 -0300

    cambio en el submodulo

---
+++ etl_scripts/party_rubro.py
@@ -0,0 +1,25 @@
+from athena2pyspark.athena_sql import querybyByName
+from athena2pyspark import run_query
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from athena2pyspark import get_dataframe
+from athena2pyspark.config import getLocalSparkSession
+
+"""
+sc = SparkContext().getOrCreate()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+"""
+
+spark = getLocalSparkSession()
+
+query = querybyByName("sql/party_rubro")
+
+temp_folder = "s3://athena2pyspark.temp/temp/"
+
+path_query = run_query(query=query, database="prod_easy", s3_output=temp_folder, spark=spark)
+
+df = get_dataframe(path_query=path_query, spark=spark)
+
+# particionar el dataframe por lo solicitado
+
