6ca683d8d4bbcdda6cc6fe5f6f1e47fcc2a6ab79
Leonardo Jofré <leonardo.jofre@gmail.com>
2017-12-13 20:50:58 -0300

    publicacion en s3

--- etl_scripts/job_carga_dinamicas.py
+++ etl_scripts/job_carga_dinamicas.py
@@ -4,22 +4,18 @@ from athena2pyspark.config import result_folder_temp, getLocalSparkSession
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 
-"""
+
 sc = SparkContext().getOrCreate()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 
 """
+spark = getLocalSparkSession()
+"""
 
 query_afinidad_marca = querybyByName("sql/afinidad_marca")
 
 query_afinidad_subclase = querybyByName("sql/afinidad_subclase")
-
-
-spark = getLocalSparkSession()
-
-
-
 
 query_afinidad_marca_path = run_query(query=query_afinidad_marca,
                                       s3_output=result_folder_temp,
@@ -34,7 +30,7 @@ query_afinidad_subclase_path = run_query(query=query_afinidad_subclase,
 df_afinidad_marca = get_dataframe(path_query=query_afinidad_marca_path, spark=spark)
 df_afinidad_subclase = get_dataframe(path_query=query_afinidad_subclase_path, spark=spark)
 
-df_afinidad_marca.write.parquet("s3://cencosud.exalitica.com/prod/jumbo/dinamicas/afinidad_marca/")
-df_afinidad_subclase.write.parquet("s3://cencosud.exalitica.com/prod/jumbo/dinamicas/afinidad_subrubro/")
+df_afinidad_marca.write.mode("Overwrite").parquet("s3://cencosud.exalitica.com/prod/jumbo/dinamicas/afinidad_marca/")
+df_afinidad_subclase.write.mode("Overwrite").parquet("s3://cencosud.exalitica.com/prod/jumbo/dinamicas/afinidad_subrubro/")
 
 pass
