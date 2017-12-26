30223fb2b4ac55bdb7cdf00ac7e4bd8ff2f1a71d
Leonardo Jofré <leonardo.jofre@gmail.com>
2017-12-26 14:32:51 -0300

    correccion menor para no agregar el getLocalSesion al etl-glue

--- etl_scripts/job_dinamica_producto_nuevo.py
+++ etl_scripts/job_dinamica_producto_nuevo.py
@@ -1,20 +1,25 @@
 # job_dinamica_producto_nuevo.py
 
+"""
+from athena2pyspark.config import getLocalSparkSession
+spark = getLocalSparkSession()
+"""
+
 import sys
+
 from awsglue.context import GlueContext
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
+
 from athena2pyspark import run_query
 from athena2pyspark.athena_sql import querybyByName
-from athena2pyspark.config import result_folder_temp, getLocalSparkSession
+from athena2pyspark.config import result_folder_temp
 
-spark = getLocalSparkSession()
 
-"""
 sc = SparkContext().getOrCreate()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
-"""
+
 
 query_str = querybyByName("sql/producto_nuevo")
 args = getResolvedOptions(sys.argv, ['id_com'])
@@ -26,4 +31,4 @@ path_query = run_query(query=query_str, database="prod_jumbo",
 
 # el resultado de la query debe quedar seteado
 
-print "El subrubro es: ", args['ID_COM']
+print "El subrubro es: ", args['id_com']
