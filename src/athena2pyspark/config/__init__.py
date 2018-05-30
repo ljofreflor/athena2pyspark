5639d5db88c61f9ae686dd8d41baa0162a96a453
Leonardo Jofré <leonardo.jofre@gmail.com>
2018-05-30 16:06:12 -0400

    eliminar .jar innecesarios

--- src/athena2pyspark/config/__init__.py
+++ src/athena2pyspark/config/__init__.py
@@ -16,7 +16,7 @@ def get_spark_session(aws_access_key_id, aws_secret_access_key, args):
 
     if args['mode'] == "local":
 
-        os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages saurfang:spark-knn:0.2.0,mysql:mysql-connector-java:5.1.38,com.amazonaws:aws-java-sdk:1.11.86,org.apache.hadoop:hadoop-aws:2.7.2 pyspark-shell'
+        os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.hadoop:hadoop-aws:2.7.3 pyspark-shell'
         spark = SparkSession.builder.master("local").getOrCreate()
         spark.conf.set("fs.s3n.awsAccessKeyId", aws_access_key_id)
         spark.conf.set("fs.s3n.awsSecretAccessKey", aws_secret_access_key)
