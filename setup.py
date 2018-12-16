6ec0827b09eabf44f18134228ea34a40eb8e0da3
Leonardo Jofré <ljofre2146@gmail.com>
2018-12-16 03:40:25 -0300

    agregar dockerfile de test

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    install_requires=["pyspark", "boto3"]
+    install_requires=["pyspark", "boto3", "configparser"]
 )
 
 # instalador
